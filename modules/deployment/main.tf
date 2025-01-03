685f3dcc470d995945311a15426511caa38b92a9
sagarbarawakar711 <114502514+sagarbarawakar711@users.noreply.github.com>
2025-01-03 14:14:18 +0530

    Update main.tf

--- modules/deployment/main.tf
+++ modules/deployment/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "make_script_executable" {
   provisioner "local-exec" {
     command = "chmod +x update_argocd.sh" 
-    working_dir = "/home/runner/work/Terraform-Docker-Image"  
+    working_dir = "/home/runner/work/Terraform-Docker-Image/Terraform-Docker-Image"  
   }
 }
 
@@ -13,6 +13,6 @@ resource "null_resource" "update_deployment_file" {
   
   provisioner "local-exec" {
     command = "./update_argocd.sh"  
-    working_dir = "/home/runner/work/Terraform-Docker-Image"
+    working_dir = "/home/runner/work/Terraform-Docker-Image/Terraform-Docker-Image"
   }
 }
